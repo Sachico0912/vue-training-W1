fc57a87c2844a782203a2e81e4806cac4e5be26f
Sachico <s104103045@gmail.com>
2025-07-31 00:41:42 +0800

    Week1_Level1_S

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+
+const items = [
+  {
+    id:1,
+    name:'珍珠奶茶',
+    description:'香濃奶茶搭配QQ珍珠',
+    price:'50',
+    quantity:'20'
+  },
+    {
+    id:2,
+    name:'冬瓜檸檬',
+    description:'清新冬瓜配上新鮮檸檬',
+    price:'45',
+    quantity:'15'
+  },
+    {
+    id:3,
+    name:'翡翠檸檬',
+    description:'	綠茶與檸檬的完美結合',
+    price:'55',
+    quantity:'30'
+  },
+    {
+    id:4,
+    name:'四季春茶',
+    description:'香醇四季春茶，回甘無比	',
+    price:'45',
+    quantity:'10'
+  },
+    {
+    id:5,
+    name:'阿薩姆奶茶',
+    description:'阿薩姆紅茶搭配香醇鮮奶',
+    price:'50',
+    quantity:'25'
+  },
+    {
+    id:6,
+    name:'檸檬冰茶	',
+    description:'檸檬與冰茶的清新組合',
+    price:'45',
+    quantity:'20'
+  },
+    {
+    id:7,
+    name:'芒果綠茶',
+    description:'芒果與綠茶的獨特風味',
+    price:'55',
+    quantity:'18'
+  },
+    {
+    id:8,
+    name:'抹茶拿鐵',
+    description:'抹茶與鮮奶的絕配',
+    price:'60',
+    quantity:'20'
+  },
+]
+
+</script>
+
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <th>序號</th>
+        <th>品項</th>
+        <th>描述</th>
+        <th>價格</th>
+        <th>庫存</th>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td>{{item.id}}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.description }}</td>
+          <td>{{ item.price }}</td>
+          <td><button>-</button>
+            {{ item.quantity }}
+            <button>+</button>
+          </td>
+        </tr>
+      </tbody>
+
+    </table>
+  </div>
+  
+</template>
+
+<style scoped>
+
+.table {
+  border:1px solid black;
+
+}
+</style>
